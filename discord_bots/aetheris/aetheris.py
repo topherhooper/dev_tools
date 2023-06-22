e1868e20a350a092e62d7ccf30510f1921662e34
Topher Hooper <hooper.christopher@gmail.com>
2023-06-22 03:35:15 +0000

    chatbot works!

--- discord_bots/aetheris/aetheris.py
+++ discord_bots/aetheris/aetheris.py
@@ -1,22 +1,39 @@
 # This example requires the 'message_content' intent.
 import discord
 from credentials import get_credentials
+from chatgpt import Brain
+import typing
 
 
 intents = discord.Intents.default()
 intents.message_content = True
+aetheris_brain = Brain(
+    system="Your name is Aetheris. You are a historian NPC within a fantasy world."
+)
+
+allowed_channel_ids = [799792721404887050]  # play-with-bots,
 
 
-class Aetheris(discord.Client):
+class DiscordBot(discord.Client):
     async def on_ready(self):
         print(f"Logged on as {self.user}!")
 
     async def on_message(self, message):
         if message.author == self.user:
             return
-        print(f"Message from {message.author}: {message.content}")
-        await self.greet(message)
-        await self.ping(message)
+
+        # Check if the message is sent in an allowed channel
+        if message.channel.id not in allowed_channel_ids:
+            return
+        if self.user.mentioned_in(message) or message.content.startswith(
+            f"{self.user.name} "
+        ):
+            print(f"Message from {message.author}: {message.content}")
+            await self.ask_aetheris(message)
+
+    async def ask_aetheris(self, message: str):
+        brain_response = aetheris_brain.get_brain_response(message=message.content)
+        await message.channel.send(brain_response)
 
     async def ping(self, message):
         await message.channel.send("Pong!")
@@ -28,9 +45,9 @@ class Aetheris(discord.Client):
 
 def main():
     credentials = get_credentials()
-    aetheris_token = credentials["Aetheris"]["token"]
-    client = Aetheris(intents=intents)
-    client.run(aetheris_token)
+    aetheris_token = credentials["Aetheris"]["discord"]
+    aetheris_client = DiscordBot(intents=intents)
+    aetheris_client.run(aetheris_token)
 
 
 if __name__ == "__main__":
--- discord_bots/aetheris/chatgpt.py
+++ discord_bots/aetheris/chatgpt.py
@@ -1,23 +1,26 @@
+from collections import namedtuple
 import openai
-import namedtuple
+import typing
+from credentials import get_credentials
 
 BrainMessage = namedtuple("BrainMessage", "user message")
 
 # count tokens:
 # https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
 
-ChatGPTModel: str = "gpt-4"
+ChatGPTModel: str = "gpt-3.5-turbo"
+# ChatGPTModel: str = "text-davinci-003"
 
 
 def num_tokens_from_string(string: str, model: str = ChatGPTModel) -> int:
     """Returns the number of tokens in a text string."""
-    encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+    encoding = tiktoken.encoding_for_model(ChatGPTModel)
     num_tokens = len(encoding.encode(string))
     return num_tokens
 
 
 class ConversationHistory:
-    def __init__(self, system=None):
+    def __init__(self, system: typing.Optional[str] = None):
         self.conversation_history = []
         self.set_system(system=system)
 
@@ -25,9 +28,10 @@ class ConversationHistory:
         self.conversation_history.append(
             {"role": "user", "content": message},
         )
+        print(message)
 
-    def set_system(self, system):
-        if not system:
+    def set_system(self, system: typing.Optional[str] = None):
+        if system is None:
             system = "You are a helpful assistant."
         # Append the new message to the conversation history
         self.conversation_history.append(
@@ -44,27 +48,18 @@ class ConversationHistory:
 
 
 class Brain:
-    def __init__(self):
-        self.conversation_history = ConversationHistory()
+    def __init__(self, system: str):
+        self.conversation_history = ConversationHistory(system=system)
+        openai.api_key = get_credentials()["Aetheris"]["openai"]
 
-    def get_brain_response(self):
+    def get_brain_response(self, message):
+        self.conversation_history.user_says(message=message)
         # Generate response using ChatGPT
-        response = openai.Completion.create(
-            engine="text-davinci-003",  # Choose the appropriate ChatGPT model
-            prompt=self.conversation_history.get(),
-            max_tokens=50,  # Adjust the max tokens as needed
+        completion = openai.ChatCompletion.create(
+            model=ChatGPTModel, messages=self.conversation_history.get()
         )
+        response = completion.choices[0].message.content
+        print(response)
 
-
-openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {
-            "role": "assistant",
-            "content": "The Los Angeles Dodgers won the World Series in 2020.",
-        },
-        {"role": "user", "content": "Where was it played?"},
-    ],
-)
+        self.conversation_history.reponse_says(response)
+        return response
